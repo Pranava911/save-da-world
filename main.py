441cfdf3c5510d39652a51dd9b0b0295f0c5b9df
Pranava911 <89236652+Pranava911@users.noreply.github.com>
2022-04-18 08:49:44 +0800

    i guess its done?

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
+servos.P2.set_angle(90)
+_continue = False
+while _continue == False:
+    basic.show_leds("""
+        # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+    """)
+    if input.logo_is_pressed():
+        _continue = True
+basic.show_leds("""
+    . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+""")
+basic.pause(1000)
+
 def on_forever():
-    
-    pass
+    if _continue == True:
+        # If dim,
+        # turn on light
+        # else if bright,
+        # turn off light
+        if pins.analog_read_pin(AnalogPin.P1) < 0:
+            servos.P2.set_angle(45)
+        elif pins.analog_read_pin(AnalogPin.P1) > 0:
+            servos.P2.set_angle(135)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
+servos.P2.setAngle(90)
+let _continue = false
+while (_continue == false) {
+    basic.showLeds(`
+        # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+    `)
+    if (input.logoIsPressed()) {
+        _continue = true
+    }
+    
+}
+basic.showLeds(`
+    . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+`)
+basic.pause(1000)
 basic.forever(function on_forever() {
+    if (_continue == true) {
+        //  If dim,
+        //  turn on light
+        //  else if bright,
+        //  turn off light
+        if (pins.analogReadPin(AnalogPin.P1) < 0) {
+            servos.P2.setAngle(45)
+        } else if (pins.analogReadPin(AnalogPin.P1) > 0) {
+            servos.P2.setAngle(135)
+        }
+        
+    }
     
 })
